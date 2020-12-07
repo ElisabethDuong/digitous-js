e6f741a0ab7bb8b7918378d2b6b097d183c72302
ElisabethDuong <elisabeth.duong@gmail.com>
2020-12-07 14:24:30 +0100

    exos 1-5 js

---
+++ jour01.js
@@ -0,0 +1,28 @@
+// 01 - Hello World 
+
+console.log("Hello World !");
+
+// 02 - String
+
+var test = "Elisabeth";
+console.log("My name is", test);
+
+// 03 - Concatenation 
+
+var name = "Elisabeth";
+console.log("Nice to meet you " + name);
+console.log(`Nice to meet you ${name}`);
+
+// 04 - String Length 
+
+var testLength = "I'm very long !".length;
+console.log(testLength);
+
+// 05 - Replace 
+
+var food = "croissant is meh";
+console.log(food.replace("meh", "so good")); 
+
+// 06 - Up and Down
+
+var basic = "This is Cool";
