0ab8c61b8503321bdebb1d3791562025521e34b0
ElisabethDuong <elisabeth.duong@gmail.com>
2020-12-08 14:18:38 +0100

    exos 1-3 js

---
+++ jour02.js
@@ -0,0 +1,23 @@
+// 01 - Number 
+
+var integer = 102;
+var float = 13.9;
+console.log("Number", integer, float);
+
+
+// 02 - Convert 
+
+var basic = 34; 
+stringified = basic.toString();
+console.log("Convert", stringified.length);
+
+
+// 03 - Round 
+
+var num = 1.5;
+var rounded = Math.round(1.5);
+console.log("Round", rounded);
+
+
+// 04 - Arithmétique
+
