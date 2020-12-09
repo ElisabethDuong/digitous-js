03e9d0e81a3e6f2bfe9247037c28ffd72355b0df
ElisabethDuong <elisabeth.duong@gmail.com>
2020-12-09 15:11:37 +0100

    exos 1-3

--- jour03.js
+++ jour03.js
@@ -1,13 +1,26 @@
 // 01 - Array 
 
-
+var fruits = ["mango", "lemon", "blueberry"];
+console.log(fruits);
+console.table(fruits); // affiche le tableau avec index et valeurs 
 
 
 // 02 - Access 
 
+var ingredients = ["eggs", "milk", "butter"];
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
+// console.log(ingredients.indexOf("coffee")); : retourne -1 car coffee n'est pas dans ma liste
 
 
 // 03 - Add and Remove 
+
+var objects = ["pen", "book", "lamp"];
+objects.unshift("chair");
+console.log(objects);
+// variable.unshift("nouvel élément") : ajoute le nouvel élément au début de la liste
+// variable.push : l'ajoute à la fin
+
 
 
 
