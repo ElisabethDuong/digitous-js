6375415e1243a1af9a30f7ac7c2cc824738d5300
ElisabethDuong <elisabeth.duong@gmail.com>
2020-12-09 16:02:45 +0100

    exos 4-5

--- jour03.js
+++ jour03.js
@@ -19,20 +19,46 @@ var objects = ["pen", "book", "lamp"];
 objects.unshift("chair");
 console.log(objects);
 // variable.unshift("nouvel élément") : ajoute le nouvel élément au début de la liste
-// variable.push : l'ajoute à la fin
 
+objects.pop();
+console.log(objects);
+// .pop() : supprime le dernier élément de la liste
 
+objects.push("laptop");
+console.log(objects);
+// .push("nouvel élément") : ajoute le nouvel élément à la fin
+
+objects.shift();
+console.log(objects);
+// .shift() : supprime le 1er élément de la liste
 
 
 // 04 - Order 
 
+var numbers = [4, 10, 8, 12, 6];
+numbers.reverse();
+console.log(numbers);
+// .reverse() : inverse l'ordre des éléments
+
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+// .sort() : tri par ordre alphabétique
+// .sort((a, b) => a - b) : par ordre croissant
+// .sort ((a, b) => b -a) : par ordre décroissant
 
 
 // 05 - Boucle 
 
+var total = 0;
+var limit = 10;
+for (var total = 0; total <= limit; total++) {
+    console.log(total);
+}
+// for (start; condition d'arrêt; incrémentation) 
 
 
 // 06 - Reverse 
+
 
 
 
