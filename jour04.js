7accae7f148fc1e478134da2e19baa391b14d41e
ElisabethDuong <elisabeth.duong@gmail.com>
2020-12-14 15:05:52 +0100

    exos 1-2 jour 4

--- jour04.js
+++ jour04.js
@@ -1,11 +1,29 @@
-// 01 - 
+// 01 - Object
+
+var cat = {
+    name: "Garfield",
+    age: 3,
+    isCute: true,
+};
+console.log(cat);
+console.log(cat.age);
+
+if (cat.isCute === true) {
+    console.log("So cuuute!!!")
+}
 
 
+// 02 - Combine
 
+var cat2 = {
+    name: "Grumpy Cat",
+    age: 5,
+    isCute: false,
+};
 
-// 02 - 
-
-
+var cats = [cat, cat2];
+console.log(cats[0].age);
+console.log(cats[1].isCute);
 
 
 // 03 - 
