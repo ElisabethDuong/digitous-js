60ed8a8ad2ba3af17b57fed49a8d682d37ae50b5
ElisabethDuong <elisabeth.duong@gmail.com>
2020-12-14 16:23:00 +0100

    exos 3-4

--- jour04.js
+++ jour04.js
@@ -6,7 +6,7 @@ var cat = {
     isCute: true,
 };
 console.log(cat);
-console.log(cat.age);
+console.log("Age:", cat.age);
 
 if (cat.isCute === true) {
     console.log("So cuuute!!!")
@@ -22,26 +22,55 @@ var cat2 = {
 };
 
 var cats = [cat, cat2];
-console.log(cats[0].age);
-console.log(cats[1].isCute);
+console.log("Garfield, age?", cats[0].age);
+console.log("Grumpy Cat, cute?", cats[1].isCute);
+// on remplace cat et cat2 par leurs index dans l'array cats
 
 
-// 03 - 
+// 03 - Even
+
+function checkIfEven(num) {
+    if (num % 2 === 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+}
+checkIfEven(2);
+checkIfEven(3);
+checkIfEven(4);
+checkIfEven(5);
 
 
+// 04 - Compare
+
+function compare(num1, num2) {
+    if (num1 > num2) {
+        console.log("num1 is bigger");
+    } else if (num2 > num1) {
+        console.log("num2 is bigger");
+    } else {
+        console.log("both are the same");
+    }
+}
+compare(7, 5);
+compare(3, 5);
+compare(5, 5);
 
 
-// 04 - 
+// 05 - Add Up
+
+function addUp(num) {
+    var sum = 0;
+    for (var i = 1; i <= num; i++) {
+        sum += i;
+        console.log(sum);
+    }
+}
+addUp(12);
 
 
-
-
-// 05 - 
-
-
-
-
-// 06 - 
+// 06 - Time
 
 
 
