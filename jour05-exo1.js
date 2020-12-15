abed3b2216420293efe1cc57a5507013cd6cd59b
ElisabethDuong <elisabeth.duong@gmail.com>
2020-12-15 15:10:57 +0100

    exo 1 jour 5

---
+++ jour05-exo1.js
@@ -0,0 +1,20 @@
+// 01 - Calculator 
+
+function calculate(num1, operator, num2) {
+    var number1 = parseInt(num1); // pr transformer le string en nombre entier
+    var number2 = parseInt(num2);
+    if (operator === "+") {
+        return number1 + number2;
+    } else if (operator === "-") {
+        return number1 - number2;
+    } else if (operator === "x") {
+        return number1 * number2;
+    } else if (operator === "/") {
+        return number1 / number2;
+    } else if (operator === "%") {
+        return number1 % number2;
+    }
+}
+// console.log(process.argv); // pr voir l'index de chaque paramètre
+console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
+// on désigne les paramètres à calculer
