09852c42f377c25a51b8e3ef650312e912b1a17f
ElisabethDuong <elisabeth.duong@gmail.com>
2021-01-04 14:40:46 +0100

    exo 2

--- jour07.js
+++ jour07.js
@@ -10,4 +10,15 @@
 
  // 02 - Map Double
 
+ var array = [1, 2, 3, 4, 5];
+
+ var double = array.map(function(num) {
+     return num * 2;
+ });
+
+ console.log(double); 
+
+
+ // 03 - Map Names
+
  
