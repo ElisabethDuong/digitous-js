0985d8569b5a883490b1193a8c6538cc6a014dc5
ElisabethDuong <elisabeth.duong@gmail.com>
2021-01-06 10:55:36 +0100

    comments added

--- jour08.js
+++ jour08.js
@@ -7,7 +7,7 @@ function getCountries() {
 
     request.get("https://restcountries.eu/rest/v2/all", function (error, response, body) {
         // console.log(error);
-        // console.log(response.statusCode);
+        console.log(response.statusCode);
         // console.log(body);
         var listCountries = JSON.parse(body);
         // console.log(listCountries);
@@ -17,16 +17,36 @@ function getCountries() {
         }
         console.log(countriesNames.join(" - "));
     });
-}
+};
 
-getCountries();
+// getCountries();
+
+
+// Autre Méthode avec .map (A REVOIR, IL Y A UNE ERREUR)
+
+// function getCountries() {
+//     var countriesNames = [];
+
+//     request.get("https://restcountries.eu/rest/v2/all", function (error, response, body) {
+//         console.log(response.statusCode);
+//         var listCountries = JSON.parse(body);
+//         var country = listCountries.map(function (element) {
+//             return element.name;
+//         })
+//         console.log(countriesNames.join(" - "));
+//     });
+// }
+
+// getCountries();
 
 
 // 02 - Chuck Norris
 
 function getFacts() {
     request.get("https://api.chucknorris.io/jokes/random", function (error, response, body) {
+        console.log(response.statusCode);
         // console.log(body);
+        // le body est un objet et on souhaite accéder à une des valeurs, on utilisera donc .value
         var fact = JSON.parse(body);
         console.log(fact.value);
     });
@@ -38,8 +58,9 @@ getFacts();
 // 03 - Pokemon
 
 function catchPokemon(id) {
-    var urlPokemon = "https://pokeapi.co/api/v2/pokemon/" + id;
-    request.get(urlPokemon, function(error, response, body) {
+    var urlPokemon = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    request.get(urlPokemon, function (error, response, body) {
+        console.log(response.statusCode);
         // console.log(body);
         var pokemon = JSON.parse(body);
         console.log(`id: ${pokemon.id}`);
@@ -50,5 +71,5 @@ function catchPokemon(id) {
 catchPokemon(25);
 
 
-// on écrit request.get("url" + id) pour définir/lier le paramètre id à l'API
+// on écrit request.get("url" + id === `url${id}` pour définir/lier le paramètre id à l'API
 // on stocke url + id dans une variable pour que ce soit plus lisible
